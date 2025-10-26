c440fbf5942333f3bc7e97c0135c9535933a8b44
Artem Petriuk <artem@example.com>
2025-10-26 02:38:20 +0200

    api fix

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,10 +1,38 @@
 import * as admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json";
+import type { ServiceAccount } from "firebase-admin";
+
+let serviceAccount: ServiceAccount;
+
+try {
+  serviceAccount = require("./serviceAccountKey.json");
+  console.log("Using local serviceAccountKey.json");
+} catch (error) {
+  console.log("No local key found. Using Environment Variables.");
+
+  if (
+    !process.env.FIREBASE_PROJECT_ID ||
+    !process.env.FIREBASE_PRIVATE_KEY ||
+    !process.env.FIREBASE_CLIENT_EMAIL
+  ) {
+    throw new Error(
+      "Firebase environment variables are not set! Please add them on Render.com"
+    );
+  }
+
+  serviceAccount = {
+    type: "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key: (process.env.FIREBASE_PRIVATE_KEY as string).replace(
+      /\\n/g,
+      "\n"
+    ),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  } as ServiceAccount;
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as any),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
-
 export default db;
