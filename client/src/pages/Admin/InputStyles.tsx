e67c4ebec7895202d2a01fe92eefe9df87587cda
endlesssjourney <fordast10@gmail.com>
2025-11-08 20:32:22 +0100

    completely change the admin page, added inputs from mui, added style variables, bit change logic of types and props, added a seperate file for input styles

---
+++ client/src/pages/Admin/InputStyles.tsx
@@ -0,0 +1,24 @@
+export const inputSx = {
+  "& .MuiInputBase-root": {
+    backgroundColor: "#1E293B",
+    color: "#F1F5F9",
+    borderRadius: "8px",
+  },
+  "& .MuiInputLabel-root": {
+    color: "#94A3B8",
+  },
+  "& .MuiInputLabel-root.Mui-focused": {
+    color: "#38BDF8",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "#334155",
+    },
+    "&:hover fieldset": {
+      borderColor: "#38BDF8",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "#38BDF8",
+    },
+  },
+};
